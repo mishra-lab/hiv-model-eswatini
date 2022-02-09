874875953a578a5bd2a0d2b87547cad5369af343
jessexknight <jesse.x.knight@gmail.com>
2022-02-09 10:58:24 -0500

    fix dict_str bug & add default n for make_targets_2020

--- code/model/target.py
+++ code/model/target.py
@@ -18,7 +18,7 @@ class Target():
       self.name,
       self.popstr(),
       self.dist.dist.name,
-      dict_str(dict(mu=self.mean(),ci=self.ci()),3),
+      dict_str(dict(mu=self.mean(),ci=self.ci())),
     )
 
   def __repr__(self):
@@ -206,7 +206,8 @@ def get_cascade_esw():
     Target(stats.beta_binom(p=.886,n= 486),'vls_c',dict(t=2016.5,s=1,    i=(0,1,2,3))),
   ]
 
-def make_targets_2020(p,n,t=2020.0,s=None,i=None,w=1):
+def make_targets_2020(p,n=None,t=2020.0,s=None,i=None,w=1):
+  if n is None: n = [100]*len(p)
   if s is None: s = (0,1)
   if i is None: i = (0,1,2,3)
   names = ['diagnosed','treated_c','vls_c']
@@ -217,12 +218,12 @@ def get_cascade_2020(which,w=1):
   # if which=='90-90-90':       return make_targets_2020([.90,.90,.90],[4259,4259,4259],w=w) # AIDSinfo
   # if which=='95-95-95':       return make_targets_2020([.95,.95,.95],[1650,1650,1650],w=w) # AIDSinfo
   if which=='ssa':            return make_targets_2020([.847,.879,.891],[31,32,32],w=w)
-  if which=='90-90-90':       return make_targets_2020([.90,.90,.90],[100,100,100],w=w)
-  if which=='95-95-95':       return make_targets_2020([.95,.95,.95],[100,100,100],w=w)
-  if which=='lower-all':      return make_targets_2020([.6,.8,.8],[100,100,100],w=w)
-  if which=='lower-fsw':      return make_targets_2020([.6,.8,.8],[100,100,100],s=0,i=(2,3),w=w) # assume
-  if which=='90-90-90-fsw':   return make_targets_2020([.90,.90,.90],[100,100,100],s=0,i=(2,3),w=w)
-  if which=='95-95-95-fsw':   return make_targets_2020([.95,.95,.95],[100,100,100],s=0,i=(2,3),w=w)
+  if which=='90-90-90':       return make_targets_2020([.90,.90,.90],w=w)
+  if which=='95-95-95':       return make_targets_2020([.95,.95,.95],w=w)
+  if which=='lower-all':      return make_targets_2020([.60,.80,.80],w=w)
+  if which=='lower-fsw':      return make_targets_2020([.60,.80,.80],s=0,i=(2,3),w=w) # assume
+  if which=='90-90-90-fsw':   return make_targets_2020([.90,.90,.90],s=0,i=(2,3),w=w)
+  if which=='95-95-95-fsw':   return make_targets_2020([.95,.95,.95],s=0,i=(2,3),w=w)
 
 def get_pop_total_esw():
   return [
