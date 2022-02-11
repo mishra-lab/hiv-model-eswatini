fd05a556e7904c37cd9183b100ea746edce1f183
jessexknight <jesse.x.knight@gmail.com>
2022-02-11 15:55:45 -0500

    refactor scenario to use worst case as REF for .red & add get_outs()

--- code/model/scenario.py
+++ code/model/scenario.py
@@ -7,15 +7,17 @@ from model import slicers,params,system,target,out,plot,fit
 
 plotsize = 3 # inches
 uid = fio.datestamp()
-cascade_low = (.60,.80,.80)
+cascade_low  = (.60,.80,.80)
+cascade_high = (.95,.95,.95)
 cases = [
+  'FSW+Cli+AQ+', # base & best
+  'FSW-Cli-AQ-', # worst
   'FSW+Cli+AQ-',
   'FSW+Cli-AQ+',
   'FSW+Cli-AQ-',
   'FSW-Cli+AQ+',
   'FSW-Cli+AQ-',
   'FSW-Cli-AQ+',
-  'FSW-Cli-AQ-',
 ]
 
 def fname(ftype,key,case,N,N0):
@@ -96,7 +98,7 @@ def get_sens_data(R,t,case):
     'EHY_acute','Rbeta_vi_rec', # HIV
     'RPA_condom_a:v','PA_circum_2050', # sex
     'RP_gud_2050','Rbeta_gud_sus_w','Rbeta_gud_inf', # GUD
-    'PX_fsw','dur_fsw_l','A_swq_cli','dur_cli', # FSW & Cli
+    'PX_fsw','dur_fsw_l','PX_cli','dur_cli', # FSW & Cli
   ]
   return dict(case=case,
     **{key:R['P'][key] for key in Pkeys},
@@ -108,13 +110,30 @@ def get_sens_data(R,t,case):
     **{
       'inc_all':  out.incidence(R,**slicers['ALL'].pop,tvec=t,t=2020)[0],
       'prev_all': out.prevalence(R,**slicers['ALL'].pop,tvec=t,t=2020)[0],
-      'prev_ratio_fsw:wq': out.vs_pop('prevalence',R,slicers['FSW'].pop,slicers['WQ'].pop,tvec=t,t=2020)[0],
-      'prev_ratio_cli:mq': out.vs_pop('prevalence',R,slicers['Cli'].pop,slicers['MQ'].pop,tvec=t,t=2020)[0],
+      'prev_ratio_fsw.wq': out.vs_pop('prevalence',R,slicers['FSW'].pop,slicers['WQ'].pop,tvec=t,t=2020)[0],
+      'prev_ratio_cli.mq': out.vs_pop('prevalence',R,slicers['Cli'].pop,slicers['MQ'].pop,tvec=t,t=2020)[0],
     }
   )
 
 def get_sens_data_n(Rs,t,case):
   return [get_sens_data(R,t,case) for R in Rs]
+
+def get_outs(Rs,t,case):
+  summary = lambda oname,Os: {
+    oname+'.mu': np.mean(Os,axis=0),
+    oname+'.md': np.median(Os,axis=0),
+    oname+'.lo': np.quantile(Os,.025,axis=0),
+    oname+'.q1': np.quantile(Os,.25, axis=0),
+    oname+'.q3': np.quantile(Os,.75, axis=0),
+    oname+'.hi': np.quantile(Os,.975,axis=0),
+  }
+  return dict(case=[case]*len(t),t=t,
+    **summary('prev_all',[out.prevalence(R,**slicers['ALL'].pop) for R in Rs]),
+    **summary('inc_all', [out.incidence(R,**slicers['ALL'].pop) for R in Rs]),
+    **summary('inc_aq',  [out.incidence(R,**slicers['AQ'].pop)  for R in Rs]),
+    **summary('inc_fsw', [out.incidence(R,**slicers['FSW'].pop) for R in Rs]),
+    **summary('inc_cli', [out.incidence(R,**slicers['Cli'].pop) for R in Rs]),
+  )
 
 def plot_diff(t,R1s,R2s,oname,snames,vsop,intervals=.95,ylim=None,xlim=None,ylab=None,**kwds):
   assert len(R1s)==len(R2s), 'R1s and R2s must have the same length'
@@ -123,7 +142,7 @@ def plot_diff(t,R1s,R2s,oname,snames,vsop,intervals=.95,ylim=None,xlim=None,ylab
   if ylab is None: ylab = out.labels.get(oname,oname)
   for i,sname in enumerate(snames): # subplots
     plot.plt.sca(ah[0,i])
-    plot.labels(slicers[sname].label,x='Time (years)',y=ylab if i==0 else None)
+    plot.labels(title=slicers[sname].label,x='Time (years)',y=ylab if i==0 else None)
     for interval in flatten(intervals):
       plot.plot_SvR(oname,t,R1s,R2s,sname,vsop=vsop,tvec=t,box=5,**kwds)
     plot.lims(y=ylim,x=xlim)
@@ -144,50 +163,54 @@ def get_refit_case(case):
     'u+': stats.uniform(l=1-eps,h=1+eps), 'u-': stats.uniform(l=1,h=25),
   }
   T2s = {
-    'FSW': target.make_targets_2020(cascade_low,s=0,i=(2,3)),
-    'Cli': target.make_targets_2020(cascade_low,s=1,i=(2,3)),
-    'AQ':  target.make_targets_2020(cascade_low,s=(0,1),i=(0,1)),
+    'FSW-': target.make_targets_2020(cascade_low ,s=0,i=(2,3)),
+    'FSW+': target.make_targets_2020(cascade_high,s=0,i=(2,3),w=1e-6),
+    'Cli-': target.make_targets_2020(cascade_low ,s=1,i=(2,3)),
+    'Cli+': target.make_targets_2020(cascade_high,s=1,i=(2,3),w=1e-6),
+    'AQ-':  target.make_targets_2020(cascade_low ,s=(0,1),i=(0,1)),
+    'AQ+':  target.make_targets_2020(cascade_high,s=(0,1),i=(0,1),w=1e-6),
   }
-  T2 = flatten(T2s[pop] for pop,c in parse_case(case) if c=='-')
+  T2 = flatten(T2s[pop+c] for pop,c in parse_case(case))
   PD = {'R'+step+'x:'+pop: PDs[step+c] for pop,c in parse_case(case) for step in 'dtu'}
   return T2,PD
 
-def main_fit(N,N0=0,sample=True,cf=True,refit=True,top=.10,sens=True,plotfit=True):
+def main_fit(N,N0=0,sample=True,refit=True,top=.10,outs=True,sens=True,plotfit=True):
   t  = system.f_t(t1=2021)
   t2 = system.f_t(t1=2050,dt=.05)
   T1 = target.get_all_esw()
   # base case (fitted)
-  case = 'base'
   if sample:
-    R1s = sample_run_base(t,T1,N,N0=N0,top=top)
-    P1s = fio.save(fname('npy','Ps',case,N,N0),[R['P'] for R in R1s])
+    R0s = sample_run_base(t,T1,N,N0=N0,top=top)
+    P0s = fio.save(fname('npy','Ps',cases[0],N,N0),[R['P'] for R in R0s])
   else:
-    P1s = fio.load(fname('npy','Ps',case,N,N0))
-  # re-run for t2
-  R1s = system.run_n(P1s,t2,T1)
-  if sens: fio.save_csv(fname('csv','sens',case,N,N0),get_sens_data_n(R1s,t2,'base'))
-  if plotfit: fit.plot_refit(t2,R1s,T1,fname('fig','fit',case,N,N0))
-  if not cf: return
+    P0s = fio.load(fname('npy','Ps',cases[0],N,N0))
+  # fio.save_csv(fname('csv','alluvial',cases[0],N,N0),out.get_sankey(R1s,t2,system.f_t(dt=1)))
   # counterfactuals (fitted)
-  for case in cases:
+  for c,case in enumerate(cases):
     print('\n'+case,flush=True)
     T2,PD = get_refit_case(case)
-    if refit:
-      P2s = fit_cascade_n(P1s,PD,T2,t,ftol=.1)
+    if c == 0:
+      P1s = P0s
+    elif refit:
+      P1s = fit_cascade_n(P0s,PD,T2,t,ftol=.1)
       fio.save(fname('npy','Ps',case,N,N0),P2s)
     else:
-      P2s = fio.load(fname('npy','Ps',case,N,N0))
+      P1s = fio.load(fname('npy','Ps',case,N,N0))
     # re-run for t2
-    R2s = system.run_n(P2s,t2,T2)
-    if sens: fio.save_csv(fname('csv','sens',case,N,N0),get_sens_data_n(R2s,t2,case))
-    if plotfit: fit.plot_refit(t2,R2s,T2,fname('fig','fit',case,N,N0))
+    R1s = system.run_n(P1s,t2,T2)
+    if outs: fio.save_csv(fname('csv','outs',case,N,N0),get_outs(R1s,t2,case))
+    if sens: fio.save_csv(fname('csv','sens',case,N,N0),get_sens_data_n(R1s,t2,case))
+    if plotfit: fit.plot_refit(t2,R1s,T2,fname('fig','fit',case,N,N0))
+    if c == 0: R0s = R1s; continue;  # save best case (for c = 1)
+    if c == 1: R2s = R1s; R1s = R0s; # save worst case (for all runs)
     # plot cumulative infections averted / extra
-    plot_diff(t2,R2s,R1s,'cuminfect',['ALL','FSW','Cli'],intervals=[.5,.95],ylim=(0,.6),
-      xlim=(1998,2042),vsop='1-2/1',ylab='Cumulative Infections Averted')
-    plot.save(fname('fig','cia3',case,N,N0))
-    plot_diff(t2,R2s,R1s,'cuminfect',['ALL','FSW','Cli'],intervals=[.5,.95],ylim=(0,.8),
+    plot_diff(t2,R1s,R2s,'cuminfect',['ALL','AQ','FSW','Cli'],intervals=[.5,.95],ylim=(0,.6),
+      xlim=(1998,2042),vsop='2-1/2',ylab='Cumulative Infections Averted')
+    plot.save(fname('fig','cia4',case,N,N0))
+    # TODO: this is zero for c==1 due to above "ifs"
+    plot_diff(t2,R1s,R0s,'cuminfect',['ALL','AQ','FSW','Cli'],intervals=[.5,.95],ylim=(0,.8),
       xlim=(1998,2042),vsop='1-2/2',ylab='Cumulative Additional Infections')
-    plot.save(fname('fig','cie3',case,N,N0))
+    plot.save(fname('fig','cie4',case,N,N0))
     # time.sleep(30) # let CPU cool :)
 
 def main_random(N,N0=0,Nr=10,sample=True,resample=True,top=.10,sens=True,plotfit=True):
