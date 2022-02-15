b09c97a86a0ca4c485cad10e629af804dabc45b4
jessexknight <jesse.x.knight@gmail.com>
2022-02-14 20:34:04 -0500

    update post/infections for inf-diff

--- code/post/infections.r
+++ code/post/infections.r
@@ -5,7 +5,7 @@ source('utils/plot.r')
 # TODO: rename waifw / alluvial & update dates + data
 
 N = 1024
-uid = '2022-02-02'
+uid = '2022-02-13'
 pops = list(
   'Women Low'  = list(clr=rgb(1.0,.60,.60)),
   'Women Med'  = list(clr=rgb(.80,.40,.40)),
@@ -27,9 +27,28 @@ clrs = list(
   to   = unname(sapply(pops, function(pop){pop$clr})),
   part = unname(sapply(parts,function(part){part$clr}))
 )
+case.defs = list(
+  'FSW-Cli+'=list(clr=rgb(.9,.3,.3),title='Left Behind: FSW'),
+  'FSW+Cli-'=list(clr=rgb(.3,.3,.9),title='Left Behind: Clients'),
+  'FSW-Cli-'=list(clr=rgb(.8,.3,.8),title='Left Behind: FSW & Clients'),
+  'FSW+Cli+'=list(clr=rgb(.9,.7,.0),title='Left Behind: Neither'))
+cases = names(case.defs)
+case.titles = unname(sapply(case.defs,function(case){case$title}))
 # --------------------------------------------------------------------------------------------------
-load.data = function(){
-  X = read.csv(root.path('data','mid',uid,paste0('sankey_base_N=0-',N-1,'.csv')))
+load.data = function(diff=FALSE){
+  if (diff){
+    which = 'infs-diff'
+  } else {
+    which = 'infs'
+    cases = c(cases,'base')
+    case.titles = c(case.titles,'Base Case')
+  }
+  X = do.call(rbind,lapply(cases,function(case){
+    X.i = read.csv(root.path('data','mid',uid,paste0(which,'_',case,'_N=0-',N-1,'.csv')))
+    X.i$case = case
+    return(X.i)
+  }))
+  X$title = factor(X$case,levels=cases,labels=case.titles)
   X = X[X$infections>0,] # for speed
   # paste hack to maintain order in alluvial
   X$from = factor(1+X$fs*4+X$fi,levels=1:8,labels=pop.labs)
@@ -63,43 +82,47 @@ do.alluvial.facet = function(X){
   g = alluvial(X[X$t %in% tvec,],norm=TRUE) +
     facet_wrap(~factor(t)) +
     theme(legend.position='top')
-  ggsave('alluvial.pdf',w=12,h=16)
 }
 do.alluvial.gif = function(X){
   save_gif(lapply(unique(X$t),function(t){ # print(t)
     print(alluvial(X[X$t==t,],norm=TRUE) + ggtitle(t))
   }),'alluvial.gif',width=600,height=800,delay=1/10,loop=TRUE)
 }
-do.margin = function(X,margin){
+do.margin = function(X,margin,rel=TRUE){
   # partnership types
-  X.abs = aggregate(formula(paste('infections ~ t +',margin)),X,sum)
+  X.abs = aggregate(formula(paste('infections ~ t + case + title +',margin)),X,sum)
   X.rel = do.norm(X.abs,sum(X.abs$t==X.abs$t[1]))
   X.abs$scale = 'Absolute (\'000s)'
   X.rel$scale = 'Proportion (%)'
-    if (margin == 'part'){ print(X.rel[X.rel$t %in% seq(1990,2040,5),]) }
+  if (margin == 'part'){ print(X.rel[X.rel$t %in% seq(1990,2040,5),]) }
   if (margin %in% c('from','to')){
     i.men = grepl('Men\\s|\\sClients',X.abs[[margin]])
     X.abs$infections[i.men] = -X.abs$infections[i.men]
   }
-  X.long = rbind(X.abs,X.rel)
+  if (rel){ X.long = rbind(X.abs,X.rel) } else { X.long = X.abs }
   g = ggplot(X.long,aes_string(x='t',y='infections',color=margin,fill=margin)) +
     geom_area(alpha=.7,size=.1) +
-    facet_wrap(vars(scale),scales='free_y') +
     scale_color_manual(values=clrs[[margin]]) +
     scale_fill_manual(values=clrs[[margin]]) +
     labs(x='Year',y='Infections',color='',fill='') +
     theme_light()
   if (margin %in% c('from','to')){ g = g + scale_y_continuous(labels=abs) }
+  if (rel){ g = g + facet_wrap(vars(scale),scales='free_y') }
   return(g)
 }
 
 # --------------------------------------------------------------------------------------------------
 X = load.data()
-do.margin(X,'part'); ggsave('inf-part.pdf',w=8,h=4)
-do.margin(X,'to');   ggsave('inf-to.pdf'  ,w=8,h=4)
-do.margin(X,'from'); ggsave('inf-from.pdf',w=8,h=4)
-do.alluvial.facet(X)
-do.alluvial.gif(X)
+X.base = X[X$case=='base',]
+do.margin(X.base,'part');  fig.save(uid,'inf-part',w=8,h=4)
+do.margin(X.base,'to');    fig.save(uid,'inf-to'  ,w=8,h=4)
+do.margin(X.base,'from');  fig.save(uid,'inf-from',w=8,h=4)
+do.alluvial.facet(X.base); fig.save(uid,'inf-alluvial',w=12,h=16)
+do.alluvial.gif(X.base)
+X.diff = load.data(diff=TRUE)
+do.margin(X.diff,'part',rel=FALSE) + facet_grid(cols=vars(title)); fig.save(uid,'inf-diff-part',w=10,h=3)
+do.margin(X.diff,'to',  rel=FALSE) + facet_grid(cols=vars(title)); fig.save(uid,'inf-diff-to',  w=10,h=3)
+do.margin(X.diff,'from',rel=FALSE) + facet_grid(cols=vars(title)); fig.save(uid,'inf-diff-from',w=10,h=3)
 
 # BUG: doesn't work as expected
 # library('gganimate')
