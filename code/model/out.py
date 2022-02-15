2cc18871d9cb455d839d27fdbd02042928336075
jessexknight <jesse.x.knight@gmail.com>
2022-02-14 20:09:07 -0500

    code: refactor get_infections to support aggrop, R2s, & vsop

--- code/model/out.py
+++ code/model/out.py
@@ -235,18 +235,22 @@ def circum(PA_circum_t,aggr=None):
 def gud(P_gud_t,aggr=None):
   return np.squeeze(P_gud_t)[:]
 
-def get_alluvial(Rs,tvec,t):
-  # get fully stratified infection counts by partnership/group for plotting sankey diagram in R
-  # TODO: stay in python? https://github.com/vinsburg/alluvial_diagram/blob/master/alluvial.py
-  sankey = [['t','p','fs','fi','ts','ti','infections']]
+def get_infections(R1s,tvec,t,aggrop=None,R2s=None,vsop='1-2'):
+  # get fully stratified infection counts by partnership/group for plotting alluvial diagram in R
+  if aggrop is None: aggrop = lambda inf: np.median(inf,axis=0)
+  if isinstance(R1s,dict): R1s = [R1s]
+  if isinstance(R2s,dict): R2s = [R2s]
+  data = [['t','p','fs','fi','ts','ti','infections']]
   for p in range(4):
     for fs in range(2):
       for fi in range(4):
         for ts in range(2):
           for ti in range(4):
             kwds = dict(tvec=tvec,t=t,p=p,fpop=dict(s=fs,i=fi),tpop=dict(s=ts,i=ti),aggr=True)
-            inf  = np.median([whoinfectwhom(R,**kwds) for R in Rs],axis=0) if isinstance(Rs,list) else\
-                   whoinfectwhom(Rs,**kwds)
-            sankey += [[tk,p,fs,fi,ts,ti,infk] for tk,infk in zip(t,inf)]
-  return sankey
-  
+            if R2s is None:
+              inf = aggrop([whoinfectwhom(R1,**kwds) for R1 in R1s])
+            else:
+              inf = aggrop([vs_fun(whoinfectwhom(R1,**kwds),
+                                   whoinfectwhom(R2,**kwds),vsop) for R1,R2 in zip(R1s,R2s)])
+            data += [[tk,p,fs,fi,ts,ti,infk] for tk,infk in zip(t,inf)]
+  return data
