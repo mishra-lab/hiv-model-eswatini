df3b09556e679a12f4eaf253cbbc9ffee6384e1e
jessexknight <jesse.x.knight@gmail.com>
2022-02-20 15:55:53 -0500

    update scinet for 2c4c3bf

--- code/scinet/main.py
+++ code/scinet/main.py
@@ -1,11 +1,16 @@
 import sys,os
-os.environ['MPLCONFIGDIR'] = sys.argv[1]
+os.environ['MPLCONFIGDIR'] = os.path.join('.tmp',sys.argv[1])
 
 from utils import parallel
 from model import scenario
 
 parallel.cpus = 80
-scenario.uid = '2022-02-16'
-scenario.N['size']  = 10000
-scenario.N['batch'] = int(sys.argv[1])
-scenario.main_fit(cf=False)
+scenario.uid = '2022-02-20'
+scenario.N['b'] = int(sys.argv[1])
+
+# scenario.run_calibrate()
+# scenario.merge_calibrate()
+scenario.run_refit()
+# scenario.merge_refit()
+# scenario.expo_refit()
+# scenario.run_sens()
