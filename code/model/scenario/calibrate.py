1b4129383e2e45955ab345857d40c00aaec6d5f0
jessexknight <jesse.x.knight@gmail.com>
2022-04-27 22:44:26 -0400

    code: fix model.calibrate merge bug from LHS seed

--- code/model/scenario/calibrate.py
+++ code/model/scenario/calibrate.py
@@ -1,7 +1,7 @@
 import numpy as np
-from utils import log
-from model import target,params,system,fit,out
-from model.scenario import *
+from utils import log,fio
+from model import target,params,system,fit
+from model.scenario import N,tvec,fname,batch_select
 
 case = 'base'
 
@@ -11,6 +11,8 @@ def run():
   T = target.get_all_esw()
   seeds = batch_select(range(N['cal']))
   P0s = params.get_n_all(len(seeds),seeds=seeds)
+  keys = ['seed']+list(params.def_sample_distrs().keys())
+  fio.save(fname('npy','sam','Ps',case=case),[{k:P[k] for k in keys} for P in P0s])
   # run & save all ll (nan for fails)
   R0s = system.run_n(P0s,t=tvec['cal'],T=T)
   fio.save_csv(fname('csv','cal','ll',case=case),
@@ -29,17 +31,8 @@ def merge():
   fio.save(fname('npy','fit','Ps',case=case,b='all'),Ps)
   # csv: get fitted params, add ll, save
   seeds = range(N['cal'])
-  P0s = params.get_n_all(N['cal'],seeds=seeds,all=False)
+  P0s = [P for b in range(N['batch']) for P in fio.load(fname('npy','sam','Ps',case=case,b=b))]
   for b in range(N['batch']):
     for ll in fio.load_csv(fname('csv','cal','ll',case=case,b=b),fmt='dict'):
       P0s[seeds.index(int(ll['seed']))].update(ll=ll['ll'])
-  fio.save_csv(fname('csv','cal','Ps',case=case,b='all'),P0s)
-
-def rerun():
-  log(0,'scenario.calibrate.rerun')
-  T = target.get_all_esw()
-  Ps = fio.load(fname('npy','fit','Ps',case=case,b='all'))
-  Rs = system.run_n(Ps,t=tvec['main'],T=T)
-  # fit.plot_cal(tvec['main'],Rs,T,fname=fname('fig','fit','plot',case=case,b='all'))
-  fio.save_csv(fname('csv','fit','infs',case=case,b='all'),
-    out.get_infections(Rs,tvec['main'],tvec['infs']))
+  fio.save_csv(fname('csv','sam','Ps',case=case,b='all'),P0s)
