f547c2d34c9e8b71239b2c780a1199255c709161
jessexknight <jesse.x.knight@gmail.com>
2022-05-06 00:11:27 -0400

    code: update scinet

--- code/scinet/main.py
+++ code/scinet/main.py
@@ -3,12 +3,14 @@ os.environ['MPLCONFIGDIR'] = os.path.join('.tmp',sys.argv[1])
 
 from utils import parallel
 import model.scenario
-from model.scenario import calibrate
+from model.scenario import calibrate,foi
 
 parallel.cpus = 80
 model.scenario.uid = '2022-04-20'
-model.scenario.N['b'] = int(sys.argv[1])
+# model.scenario.N['b'] = int(sys.argv[1])
 
-calibrate.run()
+# calibrate.run()
 # calibrate.merge()
 # calibrate.rerun()
+case = ['bpd','bpy','bmy'][int(sys.argv[1])]
+foi.run_tpaf(case)
