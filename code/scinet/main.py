6153692465caa7729e38d4579dbe1f7c1731c5de
jessexknight <jesse.x.knight@protonmail.com>
2022-10-10 14:20:23 -0400

    code: tiny scinet fix

--- code/scinet/main.py
+++ code/scinet/main.py
@@ -6,13 +6,13 @@ import model.scenario
 from model.scenario import calibrate,foi,art
 
 parallel.cpus = 80
-model.scenario.uid = '2022-09-22'
+model.scenario.uid = '2022-09-24'
 
 # calibrate ---------------------------
 # calibrate.run(int(sys.argv[1]))
 # calibrate.merge()
 # foi ---------------------------------
-# calibrate.case = 
+# calibrate.case =
 # calibrate.run(int(sys.argv[1]),foi_mode=calibrate.case)
 # calibrate.merge()
 # foi.run_ep()
