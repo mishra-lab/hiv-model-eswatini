72f6e76725fe1ad945791ac03c181dfb8ea11d6e
jessexknight <jesse.x.knight@protonmail.com>
2023-01-18 13:23:36 -0500

    code: big tweak to more-final targets

--- code/model/target.py
+++ code/model/target.py
@@ -1,6 +1,6 @@
 import numpy as np
 from inspect import signature
-from utils import stats,deco,flatten,dict_str
+from utils import deco,stats,flatten,dict_str
 from model import out
 
 class Target():
@@ -41,7 +41,7 @@ class Target():
         xlo,xhi = self.dist.interval(interval)
         return self.weight * (xlo <= x <= xhi)
       else:
-        return self.weight * self.dist.logpdf(x)
+        return self.weight * np.maximum(-1e6,self.dist.logpdf(x))
     else: # False-y
       return self.weight
 
@@ -73,13 +73,7 @@ def get_model_ll(T,R,t,interval=None):
 
 def top_ll(Rs,top=.1,ll='ll'):
   if isinstance(top,int): top = top / len(Rs)
-  # replace -np.inf with (lowest value - 1)
-  # as np.quantile has undefined behaviour (bug) for +/-infs
-  # https://github.com/numpy/numpy/issues/21091
-  Rll = np.array([R[ll] for R in Rs])
-  iRll = Rll == -np.inf
-  Rll[iRll] = Rll[~iRll].min() - 1
-  llcut = np.nanquantile(Rll,1-top)
+  llcut = np.nanquantile([R[ll] for R in Rs],1-top)
   return [R for R in Rs if R[ll] >= llcut]
 
 def get_all_esw(T=None,**kwds):
@@ -91,36 +85,35 @@ def get_all_esw(T=None,**kwds):
   T.extend(get_cascade_2020('95-95-95'))
   T.extend(get_pop_total_esw())
   # T.extend(get_prevalence_esw_anc())
-  # T.extend(get_spectrum_esw())
   return filter_targets(T,**kwds)
 
-# JK: my analysis
+# (JK): my analysis
 # SR: self-report
 
 def get_prevalence_esw():
   return [
     # TODO - check
     # prevalence ratios
-    Target(stats.ratio_binom(p1=.605,n1=127,p2=.388,n2=6015),'prevalence',dict(t=2011.0,s=0,i=(2,3)),dict(t=2011.0,s=0,i=(0,1,2,3)),vsop='1/2'), # FSW vs W
+    Target(stats.ratio_binom(p1=.605,n1=127,p2=.388,n2=6015),'prevalence',dict(t=2011.0,s=0,i=(2,3)),dict(t=2011.0,s=0,i=(0,1,2,3)),vsop='1/2',weight=2), # FSW vs W
     Target(stats.gamma(m=1.46,sd=.0842),'prevalence',dict(t=2011.0,s=0,i=3),dict(t=2011.0,s=0,i=2),vsop='1/2',weight=10), # FSW.H vs FSW.L JK
     # W.2+ vs W.0-1 & M.2+ vs M.0-1 JK
-    Target(stats.skewnorm(m=2.01,sd=.131,a=10),'prevalence',dict(t=2006.5,s=0,i=(1,2,3)),dict(t=2006.5,s=0,i=0),vsop='1/2'), # SDHS20067 (JK)
-    Target(stats.skewnorm(m=2.95,sd=.844,a=10),'prevalence',dict(t=2006.5,s=1,i=(1,2,3)),dict(t=2006.5,s=1,i=0),vsop='1/2'), # SDHS20067 (JK)
-    Target(stats.skewnorm(m=1.54,sd=.052,a=10),'prevalence',dict(t=2011.0,s=0,i=(1,2,3)),dict(t=2011.0,s=0,i=0),vsop='1/2'), # Bicego2013 (JK)
-    Target(stats.skewnorm(m=1.25,sd=.037,a=10),'prevalence',dict(t=2011.0,s=1,i=(1,2,3)),dict(t=2011.0,s=1,i=0),vsop='1/2'), # Bicego2013 (JK)
-    Target(stats.skewnorm(m=1.42,sd=.037,a=10),'prevalence',dict(t=2016.5,s=0,i=(1,2,3)),dict(t=2016.5,s=0,i=0),vsop='1/2'), # SHIMS2 (JK)
-    Target(stats.skewnorm(m=1.32,sd=.052,a=10),'prevalence',dict(t=2016.5,s=1,i=(1,2,3)),dict(t=2016.5,s=1,i=0),vsop='1/2'), # SHIMS2 (JK)
+    Target(stats.invgauss(m=2.01,sd=.129,z=1.76),'prevalence',dict(t=2006.5,s=0,i=(1,2,3)),dict(t=2006.5,s=0,i=0),vsop='1/2'), # SDHS20067 (JK)
+    Target(stats.invgauss(m=3.07,sd=.907,z=1.71),'prevalence',dict(t=2006.5,s=1,i=(1,2,3)),dict(t=2006.5,s=1,i=0),vsop='1/2'), # SDHS20067 (JK)
+    Target(stats.invgauss(m=1.54,sd=.050,z=1.42),'prevalence',dict(t=2011.0,s=0,i=(1,2,3)),dict(t=2011.0,s=0,i=0),vsop='1/2'), # Bicego2013 (JK)
+    Target(stats.invgauss(m=1.25,sd=.038,z=1.18),'prevalence',dict(t=2011.0,s=1,i=(1,2,3)),dict(t=2011.0,s=1,i=0),vsop='1/2'), # Bicego2013 (JK)
+    Target(stats.invgauss(m=1.42,sd=.036,z=1.34),'prevalence',dict(t=2016.5,s=0,i=(1,2,3)),dict(t=2016.5,s=0,i=0),vsop='1/2'), # SHIMS2 (JK)
+    Target(stats.invgauss(m=1.32,sd=.052,z=1.24),'prevalence',dict(t=2016.5,s=1,i=(1,2,3)),dict(t=2016.5,s=1,i=0),vsop='1/2'), # SHIMS2 (JK)
     # FSW
     Target(stats.betabin(p=.605,n=127),'prevalence',dict(t=2011.0,s=0,i=(2,3)),weight=10), # Baral2014
-    Target(stats.skewnorm(m=.546,sd=.056,a=-3),'prevalence',dict(t=2021.0,s=0,i=(2,3)),weight=10), # EswIBBS2022
+    Target(stats.betabin(p=.588,n=394),'prevalence',dict(t=2021.0,s=0,i=(2,3)),weight=10), # EswIBBS2022
     # SDHS2006 (Table B.2)
     Target(stats.betabin(p=.258,n=3499),'prevalence',dict(t=2006.5,s=(0,1),i=(0,1,2,3))),
     Target(stats.betabin(p=.311,n=2688),'prevalence',dict(t=2006.5,s=0,    i=(0,1,2,3))),
     Target(stats.betabin(p=.196,n=1976),'prevalence',dict(t=2006.5,s=1,    i=(0,1,2,3))),
-    # SHIMS1 Bicego2013 (Table 3) - n adjusted for sampling via '06/16 - TODO: apply 15-17 adjustment
-    Target(stats.betabin(p=.321,n=7747),'prevalence',dict(t=2011.0,s=(0,1),i=(0,1,2,3))),
-    Target(stats.betabin(p=.388,n=6015),'prevalence',dict(t=2011.0,s=0,    i=(0,1,2,3))),
-    Target(stats.betabin(p=.241,n=4977),'prevalence',dict(t=2011.0,s=1,    i=(0,1,2,3))),
+    # SHIMS1 Bicego2013 (Table 3) - n adjusted for sampling via '06/16, plus (adj 15-17)
+    Target(stats.betabin(p=.280,n=7747),'prevalence',dict(t=2011.0,s=(0,1),i=(0,1,2,3))),
+    Target(stats.betabin(p=.342,n=6015),'prevalence',dict(t=2011.0,s=0,    i=(0,1,2,3))),
+    Target(stats.betabin(p=.207,n=4977),'prevalence',dict(t=2011.0,s=1,    i=(0,1,2,3))),
     # SHIMS2 (Table C.2)
     Target(stats.betabin(p=.272,n=3620),'prevalence',dict(t=2016.5,s=(0,1),i=(0,1,2,3))),
     Target(stats.betabin(p=.343,n=2994),'prevalence',dict(t=2016.5,s=0,    i=(0,1,2,3))),
@@ -129,40 +122,33 @@ def get_prevalence_esw():
 
 def get_incidence_esw():
   return [
-    # TODO - check
     # incidence ratioss
-    Target(stats.skewnorm(m=7.51,sd=3.06,a=24),'incidence',dict(t=2011.0,s=0,i=(1,2,3)),dict(t=2011.0,s=0,i=0),vsop='1/2'), # Justman2016
-    Target(stats.skewnorm(m=5.57,sd=2.74,a=37),'incidence',dict(t=2011.0,s=1,i=(1,2,3)),dict(t=2011.0,s=1,i=0),vsop='1/2'), # Justman2016
+    Target(stats.invgauss(m=6.93,sd=3.47,z=4.18),'incidence',dict(t=2011.0,s=0,i=(1,2,3)),dict(t=2011.0,s=0,i=0),vsop='1/2'), # Justman2016
+    Target(stats.invgauss(m=4.97,sd=3.39,z=2.85),'incidence',dict(t=2011.0,s=1,i=(1,2,3)),dict(t=2011.0,s=1,i=0),vsop='1/2'), # Justman2016
     # EswIBBS2022 (Table 13)
-    Target(stats.skewnorm(m=.1284,sd=.0190),'incidence',dict(t=2021.0,s=0,i=(2,3))),
-    # SHIMS1 Justman2016 (Tables 2,1) - TODO: apply 15-17 adjustment
-    Target(stats.skewnorm(m=.0309,sd=.00281,a= 2),'incidence',dict(t=2011.0,s=0,i=(0,1,2,3))), # runtime div.by.zero error
-    Target(stats.skewnorm(m=.0170,sd=.00204,a= 0),'incidence',dict(t=2011.0,s=1,i=(0,1,2,3))),
-    Target(stats.skewnorm(m=.0321,sd=.00369,a= 3),'incidence',dict(t=2011.0,s=0,i=0)),
-    Target(stats.skewnorm(m=.0164,sd=.00355,a=10),'incidence',dict(t=2011.0,s=1,i=0)),
-    Target(stats.skewnorm(m=.1006,sd=.03834,a=12),'incidence',dict(t=2011.0,s=0,i=(1,2,3))),
-    Target(stats.skewnorm(m=.0387,sd=.00791,a= 2),'incidence',dict(t=2011.0,s=1,i=(1,2,3))),
-    # # SHIMS2 (Table 5.3.A, 5.3.B)
-    Target(stats.skewnorm(m=.0140,sd=.00321),'incidence',dict(t=2016.5,s=(0,1),i=(0,1,2,3))),
-    Target(stats.skewnorm(m=.0192,sd=.00390),'incidence',dict(t=2016.5,s=0,    i=(0,1,2,3))),
-    Target(stats.skewnorm(m=.0094,sd=.00370),'incidence',dict(t=2016.5,s=1,    i=(0,1,2,3))),
+    Target(stats.invgauss(m=.1195,sd=.0222,z=.0505),'incidence',dict(t=2021.0,s=0,i=(2,3))),
+    # SHIMS1 Justman2016 (Tables 2,1) - (adj 15-17)
+    Target(stats.skewnorm(m=.0294,sd=.00242,a=  2),'incidence',dict(t=2011.0,s=0,i=(0,1,2,3))),
+    Target(stats.skewnorm(m=.0150,sd=.00173,a=  0),'incidence',dict(t=2011.0,s=1,i=(0,1,2,3))),
+    Target(stats.skewnorm(m=.0158,sd=.00495,a=-10),'incidence',dict(t=2011.0,s=0,i=0)),
+    Target(stats.skewnorm(m=.0075,sd=.00312,a=-10),'incidence',dict(t=2011.0,s=1,i=0)),
+    Target(stats.skewnorm(m=.0970,sd=.03619,a=  8),'incidence',dict(t=2011.0,s=0,i=(1,2,3))),
+    Target(stats.skewnorm(m=.0342,sd=.00697,a=  2),'incidence',dict(t=2011.0,s=1,i=(1,2,3))),
+    # SHIMS2 (Table 5.3.A, 5.3.B)
+    Target(stats.skewnorm(m=.0148,sd=.00263),'incidence',dict(t=2016.5,s=(0,1),i=(0,1,2,3)),weight=10),
+    Target(stats.skewnorm(m=.0198,sd=.00418),'incidence',dict(t=2016.5,s=0,    i=(0,1,2,3)),weight=10),
+    Target(stats.skewnorm(m=.0099,sd=.00306),'incidence',dict(t=2016.5,s=1,    i=(0,1,2,3)),weight=10),
   ]
 
 def get_cd4_esw(w=0):
-  # TODO: beta_binom
   return [
+    Target(stats.betabin(p=.45, n=3158),'Ph',dict(t=2013.3,h=(4,5)), weight=w), # Jobanputra2015
     # SHIMS2 (Table 11.3.A)
-    Target(stats.betabin(p=.440,n=2421),'Ph',dict(t=2016.5,h=(3,4,5)),    weight=w),
-    Target(stats.betabin(p=.077,n=2421),'Ph',dict(t=2016.5,h=(5)),        weight=w),
-    Target(stats.betabin(p=.370,n=1692),'Ph',dict(t=2016.5,s=0,h=(3,4,5)),weight=w),
-    Target(stats.betabin(p=.058,n=1692),'Ph',dict(t=2016.5,s=0,h=(5)),    weight=w),
-    Target(stats.betabin(p=.591,n= 729),'Ph',dict(t=2016.5,s=1,h=(3,4,5)),weight=w),
-    Target(stats.betabin(p=.118,n= 729),'Ph',dict(t=2016.5,s=1,h=(5)),    weight=w),
+    Target(stats.betabin(p=.440,n=2421),'Ph',dict(t=2016.5,h=(3,4,5)), weight=w),
+    Target(stats.betabin(p=.077,n=2421),'Ph',dict(t=2016.5,h=(5)), weight=w),
   ]
 
 def get_cascade_esw():
-  # TODO: Jobanputra2015 (or cascade)
-  # TODO: beta_binom
   return [
     # FSW
     Target(stats.betabin(p=.744,n= 127),'diagnosed',dict(t=2011.0,s=0,i=(2,3))), # R2P2013 JK
@@ -178,15 +164,6 @@ def get_cascade_esw():
     Target(stats.betabin(p=.326,n=5742),'treated_u',dict(t=2011.0,s=(0,1),i=(0,1,2,3))), # SR
     Target(stats.betabin(p=.332,n=3810),'treated_u',dict(t=2011.0,s=0,    i=(0,1,2,3))), # SR
     Target(stats.betabin(p=.314,n=1997),'treated_u',dict(t=2011.0,s=1,    i=(0,1,2,3))), # SR
-    # Bicego2013 (SHIMS)
-    # Target(stats.betabin(p=.692,n=todo),'diagnosed',dict(t=2011.0,s=0,i=(0,1,2,3))), # SR [omit:dup]
-    # Target(stats.betabin(p=.506,n=todo),'diagnosed',dict(t=2011.0,s=1,i=(0,1,2,3))), # SR [omit:dup]
-    # Target(stats.betabin(p=.332,n=todo),'treated_u',dict(t=2011.0,s=0,i=(0,1,2,3))), # SR [omit:dup]
-    # Target(stats.betabin(p=.311,n=todo),'treated_u',dict(t=2011.0,s=1,i=(0,1,2,3))), # SR [omit:dup]
-    # EswGARPR2014
-    Target(stats.betabin(p=.527,n=100),'treated_u',dict(t=2013,s=(0,1),i=(0,1,2,3)),weight=0), # Spectrum
-    Target(stats.betabin(p=.576,n=100),'treated_u',dict(t=2013,s=0,    i=(0,1,2,3)),weight=0), # Spectrum
-    Target(stats.betabin(p=.457,n=100),'treated_u',dict(t=2013,s=1,    i=(0,1,2,3)),weight=0), # Spectrum
     # SHIMS2 (Table 10.3.A)
     Target(stats.betabin(p=.837,n=2413),'diagnosed',dict(t=2016.5,s=(0,1),i=(0,1,2,3))), # SR
     Target(stats.betabin(p=.880,n=1687),'diagnosed',dict(t=2016.5,s=0,    i=(0,1,2,3))), # SR
@@ -209,7 +186,6 @@ def get_cascade_esw():
   ]
 
 def make_targets_2020(p,n=None,t=2020.0,s=None,i=None,w=1):
-  # DONE
   if n is None: n = [100]*len(p)
   if s is None: s = (0,1)
   if i is None: i = (0,1,2,3)
@@ -218,7 +194,6 @@ def make_targets_2020(p,n=None,t=2020.0,s=None,i=None,w=1):
   return [Target(stats.betabin(p=p[i],n=n[i]),names[i],pop,weight=w) for i in range(len(p))]
 
 def get_cascade_2020(which,w=1):
-  # DONE
   if which=='90-90-90': return make_targets_2020([.90,.90,.90],w=w)
   if which=='95-95-95': return make_targets_2020([.95,.95,.95],w=w)
 
