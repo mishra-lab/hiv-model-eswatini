8800d6209bc1f1ae85c42d2c64b07c07c4fb0f16
jessexknight <jesse.x.knight@protonmail.com>
2023-03-09 18:16:23 -0500

    code: rename some post files

---
+++ code/post/main.base.r
@@ -0,0 +1,14 @@
+source('post/config.r')
+# N$sam = 1000 # DEBUG
+
+base.wiw = function(){
+  source('post/infections.r')
+  X = clean.wiw.data(read.csvs('fit','wiw','base','all'))
+  do.ratio(X);         fig.save(uid,N$sam,'wiw.base.ratio',w=5,h=5);
+  do.margin(X,'part'); fig.save(uid,N$sam,'wiw.base.part',w=5,h=5.5);
+  do.margin(X,'from'); fig.save(uid,N$sam,'wiw.base.from',w=5,h=8);
+  do.margin(X,'to');   fig.save(uid,N$sam,'wiw.base.to',  w=5,h=8);
+  do.alluvial.facet(X,seq(1990,2040,10),nrow=2); fig.save(uid,N$sam,'wiw.base.alluvial',w=8,h=10)
+}
+
+# base.wiw()
