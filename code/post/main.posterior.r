15062e6d8cb38ea6f19db072d4d6009d78427bc1
jessexknight <jesse.x.knight@protonmail.com>
2023-06-03 09:17:05 -0400

    code: overhaul post/posterior.r

--- code/post/main.posterior.r
+++ code/post/main.posterior.r
@@ -1,45 +1,44 @@
 source('post/config.r')
 
 q.cut = .01
-id.vars = c('seed','ll','ll.cut')
+id.vars = c('seed','ll','distr','case','case.lab')
+i.vars = function(X){ intersect(colnames(X),id.vars) }
+p.vars = function(X){ setdiff(colnames(X),id.vars) }
+s.post = function(X){ X$ll > quantile(X$ll,1-q.cut,na.rm=TRUE) }
 
 pre.load = function(case){
-  P = read.csv(gen.name('sam','Ps',case))
-  P$ll[!is.finite(P$ll)] = -Inf
-  P$foi_mode = NULL
-  s.post = P$ll >= quantile(P$ll,1-q.cut,na.rm=TRUE)
-  P.cor = cor(P[s.post,-which(names(P) %in% id.vars)],method='s')
-  save(P,P.cor,file=gen.name('sam','Ps',case,b='all',ext='.rdata'))
+  X = read.csv(gen.name('sam','Ps',case))
+  X$ll[!is.finite(X$ll)] = -Inf
+  X$foi_mode = NULL
+  X.cor = cor(X[s.post(X),-which(names(X) %in% id.vars)],method='s')
+  save(X,X.cor,file=gen.name('sam','Ps',case,ext='.rdata'))
 }
 
-cut.ll = function(P){
-  labs = paste(c('Bottom','Top'),sprintf('%g%%',100*c(1-q.cut,q.cut)))
-  ll.cut = cut.q(P$ll,1-q.cut,labels=labs)
+def.pp = function(X){
+  X = rbind(cbind(X,distr='Prior'),cbind(X[s.post(X),],distr='Posterior'))
 }
 
-plot.posterior = function(P,bins=32){
-  vars = setdiff(names(P),id.vars)
-  P$ll.cut = cut.ll(P)
-  P.s = split(P,P$ll.cut)
-  ksvp = unlist(lapply(vars,function(var){
-    vp = paste(var,p.stars(ks.test(P.s[[1]][[var]],P.s[[2]][[var]])$p.value))
-  }))
-  P.long = melt(P,id.vars=id.vars)
-  P.long$label = rep(ksvp,each=nrow(P))
-  g = ggplot(P.long) +
-    geom_freqpoly(aes(x=value,color=ll.cut,after_stat(density)),bins=bins) +
-    facet_wrap('label',scales='free',ncol=7) +
-    scale_color_viridis(option='inferno',discrete=TRUE,begin=0,end=.5) +
+plot.post.uni = function(X,color='distr',ncol=NULL,bins=32,stats=TRUE,p.thr=1){
+  X.long = melt(X,id.vars=i.vars(X))
+  if (stats){
+    p = ad.tests(X,color)
+    X.long = merge(X.long,cbind(p=p,p.star=sapply(p,p.stars),variable=p.vars(X)))
+    X.long$variable = paste(X.long$variable,X.long$p.star)
+    X.long = X.long[X.long$p <= p.thr,]
+  }
+  g = ggplot(X.long,aes_string(color=color)) +
+    geom_freqpoly(aes(x=value,after_stat(density)),bins=bins) +
+    stat_summary(aes(x=value,y=0),fun=mean,geom='point',shape=1,orientation='y') +
+    facet_wrap('variable',scales='free',ncol=ncol) +
     scale_x_continuous(labels=function(x){ gsub('0\\.','.',x) }) +
-    labs(x='Parameter Value',y='Density',color='Likelihood') +
-    theme_light()
+    labs(x='Parameter Value',y='Density',color='')
   g = plot.clean(g,legend.position='top',
-    panel.grid.major = element_blank(),
+    panel.grid.major=element_blank(),
     axis.text.y=element_blank(),
     axis.ticks.y=element_blank())
 }
 
-plot.cor = function(X.cor,thr=.1,rect=0){
+plot.post.cor = function(X.cor,thr=.1,rect=1){
   i.thr = apply(abs(X.cor)-diag(nrow(X.cor)) > thr,1,any) # symmetric
   X.cor = X.cor[i.thr,i.thr]
   corrplot(X.cor,method='color',order='hclust',hclust.method='ward.D2',
@@ -48,9 +47,8 @@ plot.cor = function(X.cor,thr=.1,rect=0){
     tl.cex=5/sqrt(nrow(X.cor)),tl.col='black')
 }
 
-# TODO: refactor with base.r etc.?
-case = 'base'
-# pre.load(case)
-load(file=gen.name('sam','Ps',case,b='all',ext='.rdata')) # P, P.cor
-g = plot.posterior(P); ggsave('post.distr.pdf',w=12,h=16)
-plot.cor(P.cor,thr=.2,rect=7); file.rename('Rplots.pdf','post.cor.pdf')
+ad.tests = function(X,g='distr'){
+  p.vals = par.lapply(p.vars(X),function(var){
+    p.val = kSamples::ad.test(split(X[[var]],X[[g]]))$ad[1,3]
+  })
+}
