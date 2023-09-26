7b0448815bd8a06f61f530be85ee02d74d1d0797
jessexknight <jesse.x.knight@protonmail.com>
2023-09-25 21:18:41 -0700

    code: update sjob for scinet

--- code/model/scenario/sjob.sh
+++ code/model/scenario/sjob.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name hiv-esw
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=1:00:00
-#SBATCH --array=0-9
+#SBATCH --time=0:15:00
+#SBATCH --array=0-0
 
 module load python/3.8
 # pip install --upgrade scipy pathos PyPDF2
 export PYTHONPATH=.:$PYTHONPATH
-python3.8 model/scenario/foi.py scinet case=base b=$SLURM_ARRAY_TASK_ID
+python3.8 model/scenario/imis.py scinet case=base b=$SLURM_ARRAY_TASK_ID
 
 # using this script on scinet:
 # sbatch scinet.sh
