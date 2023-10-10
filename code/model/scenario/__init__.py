2decb39e70a663a0334b780b5bbe4e76b9995ace
jessexknight <jesse.x.knight@protonmail.com>
2023-10-10 11:52:01 -0700

    code: update scenario N

--- code/model/scenario/__init__.py
+++ code/model/scenario/__init__.py
@@ -8,10 +8,11 @@ N = dict(
   batch  = 100,     # [ 100] number of batches
   bsam   = 1000,    # [1000] initial samples per batch
   isam   = 100,     # [ 100] number to resample per imis iter
-  imis   = 25,      # [  25] number of imis iterations
+  imis   = 100,     # [ 100] number of imis iterations
   post   = 1000,    # [1000] number of posterior samples
 )
-# N = dict(batch=5,bsam=100,isam=10,imis=15,post=100) # DEBUG
+# N = dict(batch=50,bsam=1000,isam=100,imis=25,post=1000) # 1000x25
+# N = dict(batch=5,bsam=100,isam=10,imis=15,post=100) # DEBUG: 5x100x15
 tvec = dict(
   cal  = system.get_t(tf=2025),
   main = system.get_t(tf=2050),
@@ -20,7 +21,7 @@ tvec = dict(
 )
 
 def fname(ftype,phase,key,case='base',b='all'):
-  subdirs = [uid,str(N['bsam'])+'x'+str(N['imis'])]
+  subdirs = [uid,str(N['bsam'])+'x'+str(N['imis'])] # TODO: add N['batch']
   if ftype=='npy':
     path,ext = ['data','npy',*subdirs],''
   if ftype=='csv':
