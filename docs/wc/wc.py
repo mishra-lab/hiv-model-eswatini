1da950459983929e35b983b807b0aac5b9e7abe2
jessexknight <jesse.x.knight@protonmail.com>
2024-01-25 10:37:29 -0500

    docs: update wc code

--- docs/wc/wc.py
+++ docs/wc/wc.py
@@ -1,6 +1,5 @@
 import os,sys,re
 # setup
-fins = sys.argv[1:]
 igs = [
   '(?<!\\\\)%.*',
   '\\\\(par|item)\\s',
@@ -26,17 +25,23 @@ reps = [
   ('\\\\cite\{(.*?)\}', '[\\1]'),
   ('\\\\href\{.*?\}\{(.*?)\}', '\\1'),
 ]
-# load
-body = ''
-for fin in fins:
-  with open(fin+'.tex','r') as f:
-    body += '\n\n<<<'+fin.upper()+'>>>\n\n'+f.read()
-# clean
-for k,v in reps:   body = re.sub(k,v,body)
-for i in igs+envs: body = re.sub(i,'',body)
-for n in range(9): body = body.replace('\\s*\n','\n').replace('\n\n\n','\n\n')
-# output
-with open('body.tmp','w') as f:
-  f.write(body)
-os.system('echo -n body:\  && wc -w body.tmp | cut -d " " -f1 && rm body.tmp')
-os.system('echo -n abs:\ \ && wc -w abstract.tex | cut -d " " -f1')
+# main
+def wc(label,files,clean=True):
+  # load
+  text = ''
+  for file in files:
+    with open(file+'.tex','r') as f:
+      text += '\n\n<<<'+file.upper()+'>>>\n\n'+f.read()
+  # clean
+  for k,v in reps:   text = re.sub(k,v,text)
+  for i in igs+envs: text = re.sub(i,'',text)
+  for n in range(9): text = text.replace('\\s*\n','\n').replace('\n\n\n','\n\n')
+  # output
+  with open('text.tmp','w') as f:
+    f.write(text)
+  os.system('echo -n \ '+label+':\ && wc -w text.tmp | cut -d " " -f1')
+  if clean:
+    os.system('rm text.tmp')
+
+wc(' abs',['abstract'])
+wc('body',sys.argv[1:])
