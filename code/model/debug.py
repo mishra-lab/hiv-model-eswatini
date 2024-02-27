911978e22f82be46c9ab0fe431ffe8621c6ee221
jessexknight <jesse.x.knight@protonmail.com>
2024-02-27 16:19:41 -0500

    code: fix windows pathos bug in model.debug

--- code/model/debug.py
+++ code/model/debug.py
@@ -1,8 +1,9 @@
 import numpy as np
 from model import system,params,target,fit
 
-t  = system.get_t(tf=2025)
-T  = target.get_all_esw()
-Ps = params.get_n_all(range(70))
-Rs = system.run_n(Ps,t,T,para=True,Xk=True)
-fit.plot_sets(t,Rs,T=T,debug=True)
+if __name__ == '__main__':
+  t  = system.get_t(tf=2025)
+  T  = target.get_all_esw()
+  Ps = params.get_n_all(range(70))
+  Rs = system.run_n(Ps,t,T,para=True,Xk=True)
+  fit.plot_sets(t,Rs,T=T,debug=True)
