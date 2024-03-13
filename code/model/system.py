0a647e54fb221f913498fcdaaea9a0a1ced074d3
jessexknight <jesse.x.knight@protonmail.com>
2024-03-13 16:28:59 -0400

    code: comment model.system

--- code/model/system.py
+++ code/model/system.py
@@ -3,15 +3,18 @@ from model import params,target,foi
 from utils import _,log,deco,parallel,rk4step
 
 def get_t(t0=1980,tf=2050,dt=0.05):
+  # define a time vector with some defaults
   return np.round(np.arange(t0,tf+dt,dt),9)
 
 @deco.nowarn
 def get_X(X0,t):
+  # initialize a population-time array X: all nan but X[0,...] = X0
   X = np.nan * np.ndarray([*t.shape,*X0.shape])
   X[0] = X0
   return X
 
 def run_n(Ps,t=None,T=None,para=True,**kwds):
+  # run the model once for each param set P in Ps, usually in parallel
   log(2,'system.run_n: N = '+str(len(Ps)))
   if para:
     fun = lambda P: run(P,t=t,T=T,**kwds)
@@ -20,52 +23,56 @@ def run_n(Ps,t=None,T=None,para=True,**kwds):
     return log(-1,[run(P,t=t,T=T,**kwds) for P in Ps])
 
 def run(P,t=None,T=None,RPts=None,Xk=False):
+  # wrapper for solve (below) with some setup & cleanup
   if t is None: t = get_t()
   if RPts is None:
+    # RPts are tarray params which we add to R to make some out.functions easier
     RPts = ['PF_condom_t','PF_circum_t','dx_sit','tx_sit','Rtx_ht','unvx_t','revx_t']
   R = solve(P,t)
   log(3,str(P['id']).rjust(9)+(' ' if R else '!'))
-  if not R:
+  if not R: # if aborted/failed for any reason
     return {'P':P,'t':t,'ll':-np.inf}
-  R['X'] = (R.get('Xk') if Xk else R.pop('Xk')).sum(axis=3) # sum_k
-  R['lls'] = target.get_model_ll(T,R,t,aggr=False) if T else {}
-  R['ll'] = sum(R['lls'].values()) if T else None
-  if RPts:
+  # sum k (EPA) dimension & possibly remove Xk to save memory
+  R['X'] = (R.get('Xk') if Xk else R.pop('Xk')).sum(axis=3)
+  R['lls'] = target.get_model_ll(T,R,t,aggr=False) if T else {} # ll per target
+  R['ll'] = sum(R['lls'].values()) if T else None # overall ll (log-likelihood)
+  if RPts: # evaluate tarray params for all t & reshape so t-dim is first
     R.update({k:np.rollaxis(P[k](t),-1) for k in RPts})
   return R
 
 #@profile
 def solve(P,t):
-  X   = get_X(P['X0'],t)
-  inc = get_X(np.zeros([4,2,4,2,4]),t)
-  b_hiv,b_tpaf = True,True
+  # solve the model for param set P & time vector t
+  X   = get_X(P['X0'],t)               # (t:*, s:2, i:4, k:5, h:6, c:5)
+  inc = get_X(np.zeros([4,2,4,2,4]),t) # (t:*, p:4, s:2, i:4, s':2, i':4)
+  b_hiv,b_tpaf = True,True # toggles so we only introduce HIV & start TPAF once
   for i in range(1,t.size):
     # Ri = rk4step(X[i-1],t[i-1],(t[i]-t[i-1]),get_dX,P=P)
     Ri = get_dX(X[i-1],t[i-1],P) # DEBUG: Euler
-    X[i] = X[i-1] + (t[i] - t[i-1]) * Ri['dX']
+    X[i] = X[i-1] + (t[i] - t[i-1]) * Ri['dX'] # X(t) = X(t-dt) + dt * dX/dt(t-dt)
     inc[i] = Ri['inc']
     if b_hiv and t[i] >= P['t0_hiv']: # introduce HIV
       b_hiv = False
       X[i,:,:,0,:,0] = X[i,:,:,0,0,0,_] * P['PX_h_hiv'][_,_,:]
-    if b_tpaf and t[i] >= P['t0_tpaf']: # start accumulating tPAF
+    if b_tpaf and t[i] >= P['t0_tpaf']: # start accumulating TPAF
       b_tpaf = False
       P['mix_mask'] = P['mix_mask_tpaf']
     if np.any(X[i].sum(axis=2) < 0) or np.any(inc[i] < 0): # abort / fail
       return False
   return {
-    'P': P,
-    'Xk': X,
-    't': t,
-    'inc': inc,
+    'P': P,     # param set
+    't': t,     # time vector
+    'Xk': X,    # population-time array (keep k dim for now)
+    'inc': inc, # incidence-time array
   }
 
 #@profile
 def get_dX(X,t,P):
-  # initialize
+  # initialize dX (fastest)
   dX = 0*X # (s:2, i:4, k:5, h:6, c:5)
-  # force of infection
+  # force of infection - modifies dX internally
   inc = foi.get_apply_inc(dX,X,t,P) # (p:4, s:2, i:4, s':2, i':4)
-  # HIV transitions
+  # HIV progression
   dXi = X[:,:,:,1:5,0:3] * P['prog_h'] # all hiv & untreated
   dX[:,:,:,1:5,0:3] -= dXi
   dX[:,:,:,2:6,0:3] += dXi
@@ -73,12 +80,12 @@ def get_dX(X,t,P):
   dXi = X[:,:,:,3:6,3:5] * P['unprog_h'] # low CD4 & treated
   dX[:,:,:,3:6,3:5] -= dXi
   dX[:,:,:,2:5,3:5] += dXi
-  # births & deaths
+  # births & deaths (entry & exit)
   birth, PXe_si, turn = params.solve_turnover(P,t)
   dX[:,:,0,0,0] += X.sum() * birth * PXe_si
   dX -= X * P['death']
   dX -= X * P['death_hc']
-  # turnover
+  # turnover among activity groups
   dXi = turn[:,:,:,_,_,_] * X[:,:,_,:,:,:]
   dX -= dXi.sum(axis=2) # (s:2, i:4, k:5, h:6, c:5)
   dX += dXi.sum(axis=1) # (s:2, i':4, k:5, h:6, c:5)
@@ -90,15 +97,15 @@ def get_dX(X,t,P):
   dXi = X[:,:,:,1:6,1] * P['tx_sit'](t) * P['Rtx_ht'](t) * P['Rtx_scen']
   dX[:,:,:,1:6,1] -= dXi # diag
   dX[:,:,:,1:6,3] += dXi # treat
-  # cascade: VLS
+  # cascade: viral suppression
   dXi = X[:,:,:,1:6,3] * P['vx']
   dX[:,:,:,1:6,3] -= dXi # treat
   dX[:,:,:,1:6,4] += dXi # vls
-  # cascade: fail
+  # cascade: treatment fail / discontinue
   dXi = X[:,:,:,1:6,4] * P['unvx_t'](t) * P['Runvx_si'] * P['Rux_scen']
   dX[:,:,:,1:6,4] -= dXi # vls
   dX[:,:,:,1:6,2] += dXi # fail
-  # cascade: re-VLS
+  # cascade: viral re-suppression
   dXi = X[:,:,:,1:6,2] * P['revx_t'](t)
   dX[:,:,:,1:6,2] -= dXi # fail
   dX[:,:,:,1:6,4] += dXi # vls
