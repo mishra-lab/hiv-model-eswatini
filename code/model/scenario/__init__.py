5770e0154870d4b2ce41850040569cce9033e8eb
jessexknight <jesse.x.knight@protonmail.com>
2024-03-13 17:04:24 -0400

    code: comment scenario.__init__

--- code/model/scenario/__init__.py
+++ code/model/scenario/__init__.py
@@ -5,21 +5,25 @@ from model import system
 uid = fio.datestamp()
 
 N = dict(
-  batch  =  100,    # [ 100] number of batches
-  hsam   = 1000,    # [1000] initial hypercube samples per batch
-  isam   =  100,    # [ 100] number to resample per imis iter
-  imis   =  100,    # [ 100] number of imis iterations
-  post   = 1000,    # [1000] number of posterior samples
+  batch =  100, # [ 100] number of batches
+  hsam  = 1000, # [1000] initial hypercube samples per batch
+  isam  =  100, # [ 100] number to resample per imis iter
+  imis  =  100, # [ 100] number of imis iterations
+  post  = 1000, # [1000] number of posterior samples
 ) # h1000i100b100
 # N = dict(batch=5,hsam=100,isam=10,imis=15,post=100) # DEBUG: h100i15b5
 tvec = dict(
-  cal  = system.get_t(tf=2025),
-  main = system.get_t(tf=2050),
-  plot = system.get_t(tf=2050,dt=1),
-  outs = system.get_t(tf=2050,t0=2000,dt=5),
+  cal  = system.get_t(tf=2025),              # for calibration
+  main = system.get_t(tf=2050),              # for main analyses
+  plot = system.get_t(tf=2050,dt=1),         # for plotting
+  outs = system.get_t(tf=2050,t0=2000,dt=5), # for (some) outputs
 )
 
 def fname(ftype,phase,key,case='base',b='all'):
+  # ftype: file type (npy, csv, fig)
+  # phase: stage of analysis pipeline (e.g. imis, fit, art-*, foi-*)
+  # key:   unique id for the thing to save / load (e.g. Ps, expo, wiw)
+  # case:  unique id for param variant used (e.g. base, foi-*, ...)
   nid = 'h{hsam}i{imis}b{batch}'.format(**N)
   subdirs = [uid,nid]
   if ftype=='npy':
@@ -32,6 +36,7 @@ def fname(ftype,phase,key,case='base',b='all'):
   return genpath(rootpath(*path,'{}_{}_{}_{}{}'.format(phase,key,case,b,ext)))
 
 def get_seeds(b):
+  # e.g. get_seeds(b=2) for N['hsam'] = 100 returns [200,...,299]
   return list(range(N['hsam']*b,N['hsam']*(b+1)))
 
 uid = '2024-03-12'
