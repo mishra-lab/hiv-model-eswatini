c0caaccbbf2ea2c9e2db5746f25b3de5e8e2b1b2
jessexknight <jesse.x.knight@protonmail.com>
2024-03-15 11:54:40 -0400

    code: remove old utils.log

--- code/utils/log.py
+++
@@ -1,8 +0,0 @@
-verb = 3
-
-def log(lvl,msg=''):
-  if lvl < 0: print(''); return msg # flush log
-  pre = ['-'*80+'\n','',' > ',''][lvl]
-  end = ['\n','\n','\n',''][lvl]
-  if lvl <= verb:
-    print(pre+str(msg),end=end,flush=True)
